ce35492996443166177530a9cbca41a0a0f0d759
ReivaxD <xavier.argentines@hotmail.com>
2026-02-15 19:07:15 +0100

    add unsat proof

--- SAT/SATChecker.py
+++ SAT/SATChecker.py
@@ -1,4 +1,6 @@
-def resolve(clauses, model):
+from pysat.solvers import Glucose3
+
+def resolveSAT(clauses, model):
     for clause in clauses:
         if not(checkClause(clause, model)):
             print("UNSAT")
@@ -18,8 +20,30 @@ def checkClause(clause, model):
     print(clause)
     return False
 
-clauses = [[1, -4], [-1, 4], [1, -3, -8], [1, 8, 12], [2, 11], [-7, -3, 9], [-7, 8, -9], [7, 8, -10], [7, 10, -12]]
+def resolveUNSAT(clauses):
+    with Glucose3(with_proof=True) as solver:
+        for clause in clauses:
+            solver.add_clause(clause)
+
+        sat = solver.solve()
+        if not sat:
+            print("UNSAT")
+            proof = solver.get_proof()
+            print("Preuve (DRAT) :")
+            print(proof)
+            for step in proof:
+                print(step)
+        else:
+            print("SAT")
+
+clause1 = [[1, 2],[-1, 3],[-2, -3]] # ie. (x1 ∨ x2) ∧ (¬x1 ∨ x3) ∧ (¬x2 ∨ ¬x3)
+clause2 = [[1, -4], [-1, 4], [1, -3, -8], [1, 8, 12], [2, 11], [-7, -3, 9], [-7, 8, -9], [7, 8, -10], [7, 10, -12]]
+clause3 = [[1],[-1, 2],[-2, 3],[-3, 4],[-1, 5],[-5, 6],[-6, 7],[-4, -7]]  # Problème CNF sans solution
+
+# Models a tester pour la clause 2
 modeltrue = {1: True, 4: True, 3: False, 8: False, 12: True, 7: True, 9: False, 2: True, 11: True, 10: False}
 modelFalse = {1: False, 4: True, 3: False, 8: False, 12: False, 7: False, 9: False, 2: False, 11: False, 10: False}
 
-print(resolve(clauses, modelFalse))
+print(resolveSAT(clause3, modelFalse))
+
+# Le résultat de resolvaUNSAT(clause3) est donné dans DRATproof.txt
